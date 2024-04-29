095c7ab78e8454f41cfa377d328bbaca63c8b9a3
Irina Carpincic <iryna.vorotynska@gmail.com>
2024-04-29 09:16:51 +0200

    Add Recipe.cookware to subgraph.

--- src/resolvers/Recipe.js
+++ src/resolvers/Recipe.js
@@ -1,11 +1,25 @@
 module.exports = {
   Recipe: {
-    ingredients(recipe, _, { dataSources }) {
+    ingredients(recipe, _, {
+      dataSources
+    }) {
       return dataSources.recipesAPI.getRecipeIngredients(recipe.id);
     },
-    relatedRecipes(recipe, _, { dataSources }) {
+    relatedRecipes(recipe, _, {
+      dataSources
+    }) {
       const limit = 3;
       return dataSources.recipesAPI.getRelatedRecipes(recipe.id, limit);
+    },
+    cookware(recipe, _, {
+      dataSources
+    }) {
+      const cookwareNamesList = dataSources.recipesAPI.getRecipeCookware(recipe.id);
+      if (!cookwareNamesList) return;
+
+      return cookwareNamesList.map((c) => ({
+        name: c,
+      }));
     },
   },
 };
